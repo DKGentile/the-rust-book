178a8fa08302d021db6ccf6223f4ecd5ba726721
Big Mench <demitgent0222@gmail.com>
2025-11-09 21:13:13 -0800

    CH6, enums

---
+++ enums/src/enumsINenums.rs
@@ -0,0 +1,73 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(UsState),
+}
+
+pub fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Quarter(state) => {
+            println!("Quarter from {state:?}!");
+            25
+        }
+        _ => {
+            println!("YOU BROKE");
+            0
+        }
+    }
+}
